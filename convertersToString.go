9d0efba1054217d897d24a14e29af09425a5fbb5
Nesles <Nesles@mail.ru>
2016-12-23 16:20:49 +0300

    -

--- convertersToString.go
+++ convertersToString.go
@@ -2,28 +2,28 @@ package liteconv
 
 import (
 	"strconv"
+//	"Kitana/dbcon"
 )
 
 const _FMT = 'f'
 
 //Float64ToString - Get string from float64
 func Float64ToString(value float64) string {
-
 	return strconv.FormatFloat(value, _FMT, 0, 64)
 }
 
-//Float32ToString - Get string from float32
+/*//Float32ToString - Get string from float32
 func Float32ToString(value float32) string {
-	return strconv.FormatFloat(value, _FMT, 0, 32)
-}
+
+}*/
 
 //Int32ToString - Get string from int32
-func Int32ToString(value int32) string  {
+func Int32ToString(value int) string  {
 	return 	strconv.Itoa(value)
 }
 
 //Int64ToString - Get string from int64
-func Int64ToString(value int64)  {
+func Int64ToString(value int64) string {
 	return strconv.FormatInt(value, 64)
 }
 
@@ -32,13 +32,15 @@ func BoolToString(value bool) string  {
 	if value{
 		return "true"
 	}
-	return false
+	return "false"
 }
 
+/*
 //Uint32ToString - get string from uint32
 func Uint32ToString(value uint32) string {
-	return strconv.FormatUint(value, 32)
+
 }
+*/
 
 //Uint64ToString - get string from uint64
 func Uint64ToString(value uint64) string {
@@ -50,23 +52,21 @@ func InterfaceToString(i interface{}) string {
 	switch value := i.(type) {
 	case string:
 		return string(value)
-	case int32:
+	case int:
 		return strconv.Itoa(value)
 	case int64:
 		return strconv.FormatInt(value, 64)
-	case uint32:
-		return strconv.FormatUint(value, 32)
+	//case uint32:
 	case uint64:
 		return strconv.FormatUint(value, 64)
-	case float32:
-		return strconv.FormatFloat(value, _FMT, 0, 32)
+	//case float32:
 	case float64:
 		return strconv.FormatFloat(value, _FMT, 0, 64)
 	case bool:
 		if value {
 			return "true"
 		}
-		return false
+		return "false"
 	default:
 		return "";
 
